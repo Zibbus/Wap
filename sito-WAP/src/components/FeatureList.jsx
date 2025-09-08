134f474fc3b10ab93a3aed729fff9617292c43cc
Flavio Furio <145505553+Zibbus@users.noreply.github.com>
2025-09-08 16:38:38 +0200

    Backend e frontend aggiornati e collegati
    Cambiata totalmente la disposizione del frontend e migliorato lo style generale.

--- sito-WAP/src/components/FeatureList.jsx
+++
@@ -1,9 +0,0 @@
-export default function FeatureList({ items = [] }) {
-    return (
-        <ul className="mt-4 space-y-2 text-gray-700">
-            {items.map((i) => (
-        <li key={i}>• {i}</li>
-            ))}
-        </ul>
-    );
-}
